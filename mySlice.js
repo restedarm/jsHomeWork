f28594844d89d5b3ce5d90d62235b4c22930d323
Arman Abrahamyan <inabr.arm@gmail.com>
2021-12-26 21:03:08 +0400

    Changed for working with all given possible arguments

--- mySlice.js
+++ mySlice.js
@@ -1,10 +1,25 @@
+
 Array.prototype.mySlice = function(start,end){
+   
+    let newArr = [];
+    start = start || 0;
+    if (end !== 0){
+        end = end || this.length;
+    }
     if (end < 0) {
         end = this.length + end;
     }
-    end = end || this.length;
-    let newArr = [];
-    
+    else if (end > this.length) {
+        end = this.length
+    }
+    if (start < 0){
+        start = this.length + start;
+    }
+
+    if ( start >= end ) {
+        console.log("empty slice ");
+        return;
+    }
     for (let i = start; i < end; i ++){
             newArr.push(this[i]);   
     }
@@ -12,6 +27,6 @@ Array.prototype.mySlice = function(start,end){
 }
 
 let arr = [1,2,3,4,5,6,7];
-console.log(arr.mySlice(3));
+console.log(arr.mySlice(0,-5));
 
 
